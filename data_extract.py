75d0be452d3a3cd7b10505b757ad639106050ace
eathandueno <eathandueno@gmail.com>
2023-11-02 17:09:29 -0600

    optimize training

--- data_extract.py
+++ data_extract.py
@@ -10,20 +10,23 @@ def get_files_in_dir(directory, extensions):
     return files
 
 # User input for directories and file types
-wiki_path = input("Enter the directory path for Wikipedia files: ")
+# wiki_path = input("Enter the directory path for Wikipedia files: ")
 folder_path = input("Enter the directory path for OpenWebText files: ")
 
 # User input for output file names
-output_file_train = input("Enter the name for the training output file (e.g., output_train.txt): ")
-output_file_val = input("Enter the name for the validation output file (e.g., output_val.txt): ")
-vocab_file = input("Enter the name for the vocabulary file (e.g., vocab.txt): ")
+# output_file_train = input("Enter the name for the training output file (e.g., output_train.txt): ")
+output_file_train = 'output_train.txt'
+output_file_val = 'output_val.txt'
+vocab_file = 'vocab.txt'
+# output_file_val = input("Enter the name for the validation output file (e.g., output_val.txt): ")
+# vocab_file = input("Enter the name for the vocabulary file (e.g., vocab.txt): ")
 
 # File extensions to look for
 wiki_extensions = ['.txt', '.csv']
 openwebtext_extensions = ['.xz']
 
 # Get files
-wiki_files = get_files_in_dir(wiki_path, wiki_extensions)
+# wiki_files = get_files_in_dir(wiki_path, wiki_extensions)
 files = get_files_in_dir(folder_path, openwebtext_extensions)
 
 # Calculate splits
--- training.py
+++ training.py
@@ -217,7 +217,7 @@ class GPTLanguageModel(nn.Module):
 
 model = GPTLanguageModel(vocab_size)
 print('loading model parameters...')
-with open('model-01.pk1', 'rb') as f:
+with open('model-01.pkl', 'rb') as f:
     model = pickle.load(f)
 print('loaded successfully')
 m = model.to(device)
